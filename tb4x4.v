2a4ea99cb8a7361a302b755f1e9f5878acbb5c4c
Christian MICHON <christian.michon@gmail.com>
2026-05-07 00:14:19 +0200

    Create tb4x4.v

---
+++ tb4x4.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ns
+
+module tb4x4;
+parameter m=4,n=4;
+integer x,y;
+wire [m+n-1:0] p;
+reg [m+n-1:0] theory;
+
+bmw4x4 multi	(
+		.x(x[m-1:0]),
+		.y(y[n-1:0]),
+		.p(p)
+		);
+
+initial
+begin 
+//$dumpfile("df");
+//$dumpvars(0,tb);
+ for(x=0;x<2**m;x=x+1)
+  begin
+    for(y=0;y<2**n;y=y+1)
+    begin
+      #1;
+      theory={{n{x[m-1]}},x[m-1:0]}*{{m{y[n-1]}},y[n-1:0]};
+      if(p!==theory)
+      $display("x=%b  y=%b  p=%b  theory=%b",x[m-1:0],y[n-1:0],p[m+n-1:0],theory[m+n-1:0]);
+    end
+  end
+  $display("%t: SIMULATION FINISHED !!!",$time);
+end
+endmodule
